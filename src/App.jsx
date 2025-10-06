6b3ee73fed8cdad4e1bc6728a9af0bb2040e2365
ARADHANA KATHROTIYA <aradhanakathrotiya@192.168.1.102>
2025-10-07 00:00:10 +0530

    6/10/25
    git push origin main

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,12 @@ import Home from "./pages/home/Home";
 
 function App() {
   return (
+<<<<<<< HEAD
     <Router>
+=======
+    
+      <Router>
+>>>>>>> eb369d2 (6/10/25)
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
